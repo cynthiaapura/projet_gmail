6dd7eaa7c75a56d125633cecf28fe887d54c9489
Cynthia Apura <120573517+cynthiaapura@users.noreply.github.com>
2024-03-10 23:49:52 +0100

    delete config.php

--- controller/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $serveur = "localhost"; // Adresse du serveur MySQL
-    $utilisateur = "root"; // Nom d'utilisateur de la base de données
-    $motDePasse = "root"; // Mot de passe de la base de données
-    $nomBaseDeDonnees = "bdd_gmail"; // Nom de la base de données
